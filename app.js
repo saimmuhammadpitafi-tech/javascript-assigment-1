27bc13feb7911093c5b994a5688047d39770ea5b
Muhammad Saim <saimmuhammadpitafi@gmail.com>
2026-08-15 08:36:25 +0500

    js assigment

---
+++ app.js
@@ -0,0 +1,220 @@
+//chapter 1 
+
+//chapter 2
+
+
+
+//var username = "Muhammad Saim"
+//alert(username)
+
+//var massage = "hello world"
+//alert(massage)
+
+//var Name = "Muhammad Saim"
+//alert(Name)
+
+//var age ="15"
+//alert(age)
+
+//var course = "Certified Mobile Application Development"
+//alert(course)
+
+//var food = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+//alert(food)
+
+//var email = " adress is example"
+//alert("My Email" + email)
+
+//var word = " I am trying to learn from the Book A Smarter way to learn JavaScript"
+//alert(word)
+
+//var text = "Yah! I can write HTML content through  JavaScript"
+//document.write(text)
+
+//var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+//alert(design)
+
+
+//chapter3
+
+
+
+//var myage = "I am 15 years old"
+//alert(myage)
+
+//var visitCount = 14;
+//alert("You have visited this site " + visitCount + " times");
+
+//var birthyear = 1990;
+//document.write ("My birth year is " + birthYear + "<br>") ;
+//document.write ("Data type of my declared variable is " + typeof(birthyear)) ;
+
+//var name = "John Doe";
+//var thing = "T-shirt(s)";
+//var num = 5;
+//document.write( name + " ordered <b>" + num  + thing + "</b> on XYZ Clothing Store");
+
+
+//chapter 4
+
+
+//var name;
+//var _name;
+//var $name;
+//var myName;
+//var my_1stVariable;
+//document.write("<h1>Rules for naming JS variables</h1>");
+
+//document.write("Variable names can only contain letters, numbers, $ and_.");
+//document.write("For example: $my_1stVariable<br>");
+//document.write("Variables must begin with a letter, $ or _.");
+//document.write("For example: $name, _name or name<br>");
+//document.write("Variable names are case sensitive.<br>");
+//document.write("Variable names should not be JS keywords.");
+
+
+//chapter 5 
+
+
+//var num1 = 3;
+//var num2 = 5;
+//var sum = num1 + num2;
+//document.write("Sum of " + num1 + " and " + num2 + " is " + sum);
+
+
+//var num1 = 3;
+//var num2 = 5;
+
+//var subtraction = num1 - num2;
+//document.write("Subtraction of " + num1 + " and " + num2 + " = " + subtraction + "<br>");
+
+//var multiplication = num1 * num2;
+//document.write("Multiplication of " + num1 + " and " + num2 + "=" + multiplication + "<br>");
+
+//var division = num1 / num2;
+//document.write("Division of " + num1 + " and " + num2 + "= " + division + "<br>");
+
+//var modulus = num1 % num2;
+//document.write("Modulus of " + num1 + " and " + num2 + " = " + modulus);
+
+
+//var num;
+
+//document.write("Value after variable declaration is undefined." +  "<br>");
+
+//num = 5;
+
+//document.write("Initial value: " + num + "<br>");
+
+//num++;
+
+//document.write("Value after increment is: " + num + "<br>");
+
+//num = num + 7;
+
+//document.write("Value after addition is: " + num + "<br>");
+
+//num--;
+
+//document.write("Value after decrement is: " + num + "<br>");
+
+//var remainder = num % 3;
+
+//document.write("The remainder is: " + remainder);
+
+
+//var num = 5; 
+
+//document.write(num + " x 1 = " + (num * 1) + "<br>");
+//document.write(num + " x 2 = " + (num * 2) + "<br>");
+//document.write(num + " x 3 = " + (num * 3) + "<br>");
+//document.write(num + " x 4 = " + (num * 4) + "<br>");
+//document.write(num + " x 5 = " + (num * 5) + "<br>");
+//document.write(num + " x 6 = " + (num * 6) + "<br>");
+//document.write(num + " x 7 = " + (num * 7) + "<br>");
+//document.write(num + " x 8 = " + (num * 8) + "<br>");
+//document.write(num + " x 9 = " + (num * 9) + "<br>");
+//document.write(num + " x 10 = " + (num * 10));
+
+
+//var celsius = 25;
+//var fahrenheit = (celsius * 9 / 5) + 32;
+//document.write(celsius + "°C is " + fahrenheit + "°F<br><br>");
+//var fahrenheit2 = 70;
+//var celsius2 = (fahrenheit2 - 32) * 5 / 9;
+//document.write(fahrenheit2 + "°F is " + celsius2 + "°C");
+
+//var price = "Price of item 1 is 650 <br>"
+//document.write(price)
+//var bnb = "Quantity of item 1 is 3 <br>"                        
+//document.write(bnb)
+//var priz = "Price of item 2 is 100 <br>"
+//document.write(priz)
+//var quan = "Quantity of item 2 is 7 <br>"   
+//document.write(quan)
+//var ship = "Shiping of charges 100 <br>"
+//document.write(ship)
+//var cost = "TOTAL codt of your order is 2750 "
+//document.write(cost)
+
+
+//var mark = "Totak marks: 980 <br>"
+//document.write(mark)
+//var obt = "Marks Obtained: 804 <>br"
+//document.write(obt)
+//var per = "Percentage:82.0408163265306%"
+//document.write(per)
+
+
+//var cur = "Total currency in PKR 1748"
+//document.write(cur)
+
+
+//var num = 10;
+//var result = ((num + 5) * 10) / 2;
+//document.write("Initial Number: " + num + "<br>");
+//document.write("Result: " + result);
+
+//var date = "Current Year: 2016 <br>"
+//var by = "Birth Year: 1992 <br>"
+//var youage = "Your age is: 24 <br>"
+//document.write(date)
+//document.write(by)
+//document.write(youage)
+
+//var radius = 5;
+//var pi = 3.142;
+
+//var circumference = 2 * pi * radius;
+//var area = pi * radius * radius;
+
+//document.write("The circumference is " + circumference + "<br>");
+//document.write("The area is " + area);
+
+
+//var favoriteSnack = "Chips";
+//var currentAge = 20;
+//var maximumAge = 70;
+//var amountPerDay = 2;
+
+//var yearsRemaining = maximumAge - currentAge;
+//var totalSupply = yearsRemaining * 365 * amountPerDay;
+
+//document.write(
+    //"You will need " + totalSupply + " " + favoriteSnack +
+    //" to last you until the ripe old age of " + maximumAge);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
